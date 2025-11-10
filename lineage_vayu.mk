30b2927e94b3a4ab5ef93ab5108c278f9030eaf4
momenabdulrazek <momen.abdelrazek33@gmail.com>
2025-11-10 13:23:02 +0000

    Get ready @AviumUI !

--- lineage_vayu.mk
+++ lineage_vayu.mk
@@ -14,6 +14,9 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 # Inherit from vayu device
 $(call inherit-product, device/xiaomi/vayu/device.mk)
 
+# Inherit AviumUi flags
+$(call inherit-product, device/xiaomi/vayu/avium_common.mk)
+
 PRODUCT_NAME := lineage_vayu
 PRODUCT_DEVICE := vayu
 PRODUCT_MANUFACTURER := Xiaomi
