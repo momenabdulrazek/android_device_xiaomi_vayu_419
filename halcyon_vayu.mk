9ff41f4b782ba68c131eba014754101cc3a6a443
momenabdulrazek <momen.abdelrazek33@gmail.com>
2025-11-16 04:22:59 +0000

    test: halcyon adaptations

--- halcyon_vayu.mk
+++ halcyon_vayu.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit common AOSP configurations
-$(call inherit-product, vendor/halcyon/config/common_full_phone.mk)
+$(call inherit-product, vendor/halcyon/config/common.mk)
 
 # Inherit from vayu device
 $(call inherit-product, device/xiaomi/vayu/device.mk)
@@ -17,13 +17,14 @@ $(call inherit-product, device/xiaomi/vayu/device.mk)
 # Boot animation
 TARGET_BOOT_ANIMATION_RES := 1080
 
-PRODUCT_NAME := aosp_vayu
+PRODUCT_NAME := halcyon_vayu
 PRODUCT_DEVICE := vayu
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := Poco X3 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+WITH_GMS := true
 
 PRODUCT_MODEL := M2102J20SG
 
